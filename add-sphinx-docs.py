5e6dcc65758f1962635284b64681d5f229ab699e
Erin Le <ejle@ucdavis.edu>
2024-11-25 16:08:02 -0800

    website: Add Sphinx documentation to website using GitHub Actions

--- add-sphinx-docs.py
+++ add-sphinx-docs.py
@@ -1,14 +1,24 @@
 import os
 
-# for filename in os.listdir('../../docs/_build/html'):
-# for filename in os.listdir('/Volumes/Crucial/gem5-dev/website/_pages/documentation/general_docs/sphinx_docs'):
 search_bar_lines = []
 html = []
 
-with open(f"./_build/html/_modules/index.html", "r+") as f:
+def remove_searchbar(html):
+    search_flag = False
+    for index, line in enumerate(html):
+        if '<div id="searchbox" style="display: none" role="search">' in line:
+            search_flag = True
+        if search_flag == True:
+            html[index] = ""
+        if '<script>document.getElementById(\'searchbox\').style.display = "block"</script>' in line:
+            search_flag = False
+    return html
+
+
+with open(f"./_pages/documentation/general_docs/sphinx_docs/_modules/index.html", "r+") as f:
     html = f.readlines()
     f.seek(0, 0)
-
+    f.truncate(0)
     # Add frontmatter so the docs pages can be accessed in the website
     f.write("---\n")
     f.write(f'title: "Sphinx Documentation"\n')
@@ -16,7 +26,6 @@ with open(f"./_build/html/_modules/index.html", "r+") as f:
     f.write(f"permalink: /documentation/general_docs/sphinx_docs/index.html\n")
     f.write("---\n")
 
-    search_flag = False
     for index, line in enumerate(html):
         modified_line = None
 
@@ -30,77 +39,49 @@ with open(f"./_build/html/_modules/index.html", "r+") as f:
                     '<li><a href="./index.html">Documentation overview</a><ul>'
                 )
             else:
-                # For links to all Sphinx documentation pages from index, replace the `.` in the path with `/`. This makes things more consistent and allows both links from index.html to a Sphinx page and links between two Sphinx pages to work.
+                # For links to the other documentation pages from index.html, replace the `.` in the path with `/`. This makes things more consistent and allows links from index.html to a Sphinx page and links between two Sphinx pages to work.
                 modified_line = (
                     line.replace("/", ".")
                     .replace("<.a>", "</a>")
                     .replace("<.li>", "</li>")
                 )
-        if (
+        elif (
             '<li class="toctree-l1"><a class="reference internal" href="../gem5.html">gem5 package</a></li>'
             in line
         ):
             print("../gem5.html switched to ./gem5.html")
             modified_line = '<li class="toctree-l1"><a class="reference internal" href="./gem5.html">gem5 package</a></li>'
 
-        if '<h1 class="logo"><a href="../index.html">gem5</a></h1>' in line:
+        elif '<h1 class="logo"><a href="../index.html">gem5</a></h1>' in line:
             print("../index.html switched to ./index.html")
             modified_line = (
                 '<h1 class="logo"><a href="./index.html">gem5</a></h1>'
             )
-
-        # remove the search bar
-        if 'id="searchbox"' in line:
-            search_flag = True
-            modified_line = ""
-        elif "'searchbox').style.display = \"block\"</script>" in line:
-            search_flag = False
-            modified_line = ""
-        elif search_flag == True:
-            modified_line = ""
         else:
             modified_line = line
-        # if '</html>searchbox\').style.display = "block"</script>' in line:
-        #     f.write("</html>")
-        # else:
-        # f.write(modified_line)
+
         html[index] = modified_line
-        # print("latest version gotten")
 
-    # with open(f"./_build/html/_modules/index.html", "r+") as f:
-    # html = f.readlines()
-    # f.seek(0, 0)
-    for index, line in enumerate(html):
-        if '</html>searchbox\').style.display = "block"</script>' in line:
-            # f.write("</html>")
-            html[index] = "</html>"
-        # else:
-        #     f.write(line)
-    f.write(html)
+    html = remove_searchbar(html)
+    
+    for line in html:
+        f.write(line)
 
 
-for filename in os.listdir("./_build/html"):
-
-    # print(filename)
-    if filename.startswith("gem5"):  # and filename != "gem5.html"
-        # print(filename)
-        # with open (f"../../docs/_build/html/{filename}", "r+") as f:
-        # with open (f"/Volumes/Crucial/gem5-dev/website/_pages/documentation/general_docs/sphinx_docs/{filename}", "r+") as f:
-        with open(f"./_build/html/{filename}", "r+") as f:
-            html = f.read()
+for filename in os.listdir("./_pages/documentation/general_docs/sphinx_docs"):
+    if filename.startswith("gem5"):
+        with open(f"./_pages/documentation/general_docs/sphinx_docs/{filename}", "r+") as f:
+            html = f.readlines()
             f.seek(0, 0)
+            f.truncate(0)
             f.write("---\n")
             f.write(f'title: "{filename}"\n')
             f.write("parent: sphinx-docs\n")
-            # modified_filename = filename.replace(".", "/").replace(
-            #     "/html", ".html"
-            # )
-
-            # f.write(
-            #     f"permalink: /documentation/general_docs/sphinx_docs/{modified_filename}\n"
-            # )
             f.write(
                 f"permalink: /documentation/general_docs/sphinx_docs/{filename}\n"
             )
             f.write("---\n")
-            f.write(html)
+            html = remove_searchbar(html)
+            
+            for line in html:
+                f.write(line)

51a9707c57640343222e778ebb8e6df329dd0b2b
Erin Le <ejle@ucdavis.edu>
2024-12-10 21:33:54 -0800

    website: make requested changes
    This commit changes the path to files to be committed and removes
    the caching step from sphinx-to-website.yaml. It also makes
    formatting changes to add-sphinx-docs.py and adds a copyright
    header.

--- add-sphinx-docs.py
+++ add-sphinx-docs.py
@@ -1,7 +1,34 @@
+# Copyright (c) 2024 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 
 search_bar_lines = []
 html = []
+
 
 def remove_searchbar(html):
     search_flag = False
@@ -10,12 +37,18 @@ def remove_searchbar(html):
             search_flag = True
         if search_flag == True:
             html[index] = ""
-        if '<script>document.getElementById(\'searchbox\').style.display = "block"</script>' in line:
+        if (
+            "<script>document.getElementById('searchbox').style.display = \"block\"</script>"
+            in line
+        ):
             search_flag = False
     return html
 
 
-with open(f"./_pages/documentation/general_docs/sphinx_docs/_modules/index.html", "r+") as f:
+with open(
+    f"./_pages/documentation/general_docs/sphinx_docs/_modules/index.html",
+    "r+",
+) as f:
     html = f.readlines()
     f.seek(0, 0)
     f.truncate(0)
@@ -23,8 +56,9 @@ with open(f"./_pages/documentation/general_docs/sphinx_docs/_modules/index.html"
     f.write("---\n")
     f.write(f'title: "Sphinx Documentation"\n')
     f.write("parent: sphinx-docs\n")
-    f.write(f"permalink: /documentation/general_docs/stdlib_api/\n")
+    f.write(f"permalink: documentation/general_docs/stdlib_api\n")
     f.write("---\n")
+
     for index, line in enumerate(html):
         modified_line = None
 
@@ -62,14 +96,16 @@ with open(f"./_pages/documentation/general_docs/sphinx_docs/_modules/index.html"
         html[index] = modified_line
 
     html = remove_searchbar(html)
-    
+
     for line in html:
         f.write(line)
 
 
 for filename in os.listdir("./_pages/documentation/general_docs/sphinx_docs"):
     if filename.startswith("gem5"):
-        with open(f"./_pages/documentation/general_docs/sphinx_docs/{filename}", "r+") as f:
+        with open(
+            f"./_pages/documentation/general_docs/sphinx_docs/{filename}", "r+"
+        ) as f:
             html = f.readlines()
             f.seek(0, 0)
             f.truncate(0)
@@ -77,10 +113,10 @@ for filename in os.listdir("./_pages/documentation/general_docs/sphinx_docs"):
             f.write(f'title: "{filename}"\n')
             f.write("parent: sphinx-docs\n")
             f.write(
-                f"permalink: /documentation/general_docs/stdlib_api/{filename}\n"
+                f"permalink: /documentation/general_docs/sphinx_docs/{filename}\n"
             )
             f.write("---\n")
             html = remove_searchbar(html)
-            
+
             for line in html:
                 f.write(line)
